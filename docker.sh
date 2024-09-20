82841d430705239b04e85d7c55557b6959f20d2b
sillygir1 <25554397+sillygir1@users.noreply.github.com>
2024-09-21 00:04:28 +0400

    Update docker.sh

--- docker.sh
+++ docker.sh
@@ -6,12 +6,12 @@ elif [ "$1" == 'build' ]
 then
     time docker exec -it duodocker /bin/bash -c "cd /home/work/ && rm -rf buildroot-2021.05/output/milkv-duo256m_musl_riscv64/build/proxmark3* && rm -rf buildroot-2021.05/output/milkv-duo256m_musl_riscv64/build/encoder* && ./build.sh milkv-duo256m-sd"
     notify-send -a "Docker" -u critical -w "Run finished"
-elif [ "$1" == 'clean' ]
+elif [ "$1" == 'clean-dev' ]
 then
     docker exec -it duodocker /bin/bash -c "cd /home/work/ && rm -rf buildroot-2021.05/output/milkv-duo256m_musl_riscv64/build/proxmark3*"
 elif [ "$1" == 'clean-pkg' ]
 then
     docker exec -it duodocker /bin/bash -c "cd /home/work/ && rm -rf buildroot-2021.05/output/milkv-duo256m_musl_riscv64/build/"
 else
-    echo "Avaliable options: build, clean, clean-pkg, set-defconfig"
+    echo "Avaliable options: build, clean-dev, clean-pkg, set-defconfig"
 fi
