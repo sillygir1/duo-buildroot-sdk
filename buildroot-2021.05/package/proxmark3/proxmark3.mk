f36f64010831b4017b75a70b9904f13b53a98106
sillygir1 <25554397+sillygir1@users.noreply.github.com>
2024-09-21 15:54:49 +0400

    Update proxmark3 recipe

--- buildroot-2021.05/package/proxmark3/proxmark3.mk
+++ buildroot-2021.05/package/proxmark3/proxmark3.mk
@@ -4,7 +4,7 @@
 #
 ################################################################################
 
-PROXMARK3_VERSION = master
+PROXMARK3_VERSION = c7af82f146180dc2655a529854dda07149b98501
 PROXMARK3_SITE = https://github.com/RfidResearchGroup/proxmark3
 PROXMARK3_SITE_METHOD = git
 
@@ -12,20 +12,16 @@ PROXMARK3_DEPENDENCIES = bzip2 lz4 lua
 
 PROXMARK3_INSTALL_TARGET = YES
 
-BZIP2_PATH = /home/work/buildroot-2021.05/output/milkv-duo256m_musl_riscv64/build/bzip2-1.0.8/
-LZ4_PATH = /home/work/buildroot-2021.05/output/milkv-duo256m_musl_riscv64/build/lz4-1.9.3/lib/
+LIBS_DIR = $(STAGING_DIR)/usr/lib
 
 SUBST_CFLAGS = CFLAGS="-D_LARGEFILE_SOURCE -DNOSIMD_BUILD -I$(@D)/include -I$(@D)/client/src -I$(@D)/common -I$(@D)/common/mbedtls -I$(@D)/client/deps/jansson/ -I$(@D)/client/deps/whereami/
-TEMP_OPTS = $(subst CFLAGS="-D_LARGEFILE_SOURCE,$(SUBST_CFLAGS),$(TARGET_CONFIGURE_OPTS))
+PM3_OPTS = $(subst CFLAGS="-D_LARGEFILE_SOURCE,$(SUBST_CFLAGS),$(TARGET_CONFIGURE_OPTS))
 
-SUBST_CFLAGS_BUILD = CFLAGS_FOR_BUILD="-O2 -I$(BZIP2_PATH) -I$(LZ4_PATH)
-PM3_OPTS_2 = $(subst CFLAGS_FOR_BUILD="-O2,$(SUBST_CFLAGS_BUILD),$(TEMP_OPTS))
-
-LDLIBS = LDLIBS:=-static LDLIBS+=$(LZ4_PATH)liblz4.a LDLIBS+=$(BZIP2_PATH)libbz2.a
+LDLIBS = LDLIBS:=-static LDLIBS+=$(LIBS_DIR)/liblz4.a LDLIBS+=$(LIBS_DIR)/libbz2.a
 
 define PROXMARK3_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D) clean	
-	$(MAKE) $(PM3_OPTS_2) $(LDLIBS) AR="${AR} rcs" -C $(@D) client cpu_arch=generic SKIPWHEREAMISYSTEM=1 SKIPREVENGTEST=1 SKIPBT=1 SKIPQT=1 SKIPREADLINE=1 SKIPJANSSONSYSTEM=1 SKIPLINENOISE=1 SKIPPYTHON=1 SKIPGD=1 -j
+	$(MAKE) $(PM3_OPTS) $(LDLIBS) AR="${AR} rcs" -C $(@D) client cpu_arch=generic SKIPWHEREAMISYSTEM=1 SKIPREVENGTEST=1 SKIPBT=1 SKIPQT=1 SKIPREADLINE=1 SKIPJANSSONSYSTEM=1 SKIPLINENOISE=1 SKIPPYTHON=1 SKIPGD=1 -j
 endef
 
 define PROXMARK3_INSTALL_TARGET_CMDS
